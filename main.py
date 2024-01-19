f45ef1940331a55df28c180e2aed6538b81163b0
hamidreza <dolati.hamidreza78@gmail.com>
2024-01-19 16:24:19 +0330

    understood how .pack() works, never gonna use .place() let's move on to .grid()

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ from tkinter import ttk
 root = tk.Tk()
 frm = ttk.Frame(root, padding=10)
 # frm.grid()
-frm.pack()
+frm.pack(fill=tk.BOTH, expand=True)
 
-label = ttk.Label(master=frm, text="Hello World") #.grid(column=0, row=0)
+label = ttk.Label(master=frm, text="Hello World", anchor="center") #.grid(column=0, row=0)
 text = tk.Text(master=frm)
-button = ttk.Button(master=frm, text="Quit", command=root.destroy) #.grid(column=2, row=0)
 
-label.pack()
-text.pack(fill=tk.X)
-button.pack()
+btn_frm = ttk.Frame(master=frm)
+button = ttk.Button(master=btn_frm, text="Quit", command=root.destroy) #.grid(column=2, row=0)
+
+label.pack(fill=tk.BOTH)
+text.pack(fill=tk.BOTH, side=tk.LEFT)
+btn_frm.pack(fill=tk.BOTH)
+
+button.pack(padx=5)
+
+root.resizable(width=False, height=False)
 
 root.mainloop()
