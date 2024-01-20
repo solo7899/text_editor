30ebe61771f90894b636a29f08824981c100a2fc
hamidreza <dolati.hamidreza78@gmail.com>
2024-01-20 19:19:12 +0330

    adding openfile function/button

--- funcs.py
+++ funcs.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 
-def save_file(text):
+def save_file(text, label):
     """this fucntion is responsible for savedialago and file saving
 
     Args:
@@ -13,6 +13,18 @@ def save_file(text):
             with open(file_path, "w") as file:
                 text_content = text.get("1.0", tk.END)
                 file.write(text_content)
+                label["text"] = file_path.split("/")[-1]
         except Exception as e:
             print(f"Error saving file {str(e)}")
 
+
+def open_file(text, label):
+    file_path = filedialog.askopenfilename(filetypes=(["text files", "*.txt"], ["all files", "*.*"]))
+    if file_path:
+        try:
+            with open(file_path, "r") as file:
+                file_context = file.read()
+                text.insert("1.0", file_context)
+                label["text"] = file_path.split("/")[-1]
+        except Exception as e:
+            print(f"Error reading file {0}", str(e))
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk, font
 import sys
 
-from funcs import save_file
+from funcs import save_file, open_file
 
 #makeing the main window
 main_window = tk.Tk()
@@ -15,13 +15,14 @@ frm.pack(fill="both" ,side="top", expand=True)
 
 #setting main Frame objects
 fontObj = font.Font(size=18)
-label = ttk.Label(master=frm, text="Hello World", anchor="center") #.grid(column=0, row=0)
+label = ttk.Label(master=frm, text="", anchor="center") #.grid(column=0, row=0)
 text = tk.Text(master=frm, font=fontObj)
 
 #setting btnFrame and its objects
 btn_frm = ttk.Frame(master=frm)
 Q_button = ttk.Button(master=btn_frm, text="Quit", command=main_window.destroy) #.grid(column=2, row=0)
-S_button = ttk.Button(master=btn_frm, text="Save", command=lambda: save_file(text)) #.grid(column=2, row=0)
+S_button = ttk.Button(master=btn_frm, text="Save", command=lambda: save_file(text, label)) #.grid(column=2, row=0)
+O_button = ttk.Button(master=btn_frm, text="Open", command=lambda: open_file(text, label))
 
 label.pack(fill="both")
 text.pack(fill="both", side="top", expand=True)
@@ -33,6 +34,7 @@ text.focus()
 btn_frm.pack(fill="both", side="top", expand=True)
 Q_button.grid(padx=5, pady=5, sticky="w", column=0, row=0)
 S_button.grid(column=1, row=0, padx=5, pady=5)
+O_button.grid(column=2, row=0, padx=5, pady=5)
 
 
 main_window.resizable(width=False, height=False)
