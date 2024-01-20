7eedbdade34f32e9512fb8c308845bbb1dc281cb
hamidreza <dolati.hamidreza78@gmail.com>
2024-01-20 13:50:31 +0330

    adding save function/button

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import tkinter as tk 
 from tkinter import ttk, font
+from tkinter import filedialog
+import sys
 
-root = tk.Tk()
-frm = ttk.Frame(root, padding=10)
+def save_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("text file", "*.txt"), ("all files", "*.*")])
+    if file_path:
+        try:
+            with open(file_path, "w") as file:
+                text_content = text.get("1.0", tk.END)
+                file.write(text_content)
+        except Exception as e:
+            print(f"Error saving file {str(e)}")
+
+main_window = tk.Tk()
+main_window.bind("<Escape>" , sys.exit)
+frm = ttk.Frame(main_window, padding=10)
 # frm.grid()
 frm.pack(fill="both" ,side="top", expand=True)
 
@@ -12,12 +25,13 @@ label = ttk.Label(master=frm, text="Hello World", anchor="center") #.grid(column
 text = tk.Text(master=frm, font=fontObj)
 
 btn_frm = ttk.Frame(master=frm)
-Q_button = ttk.Button(master=btn_frm, text="Quit", command=root.destroy) #.grid(column=2, row=0)
-S_button = ttk.Button(master=btn_frm, text="Save") #.grid(column=2, row=0)
+Q_button = ttk.Button(master=btn_frm, text="Quit", command=main_window.destroy) #.grid(column=2, row=0)
+S_button = ttk.Button(master=btn_frm, text="Save", command=save_file) #.grid(column=2, row=0)
 
 
 label.pack(fill="both")
 text.pack(fill="both", side="top", expand=True)
+text.focus()
 
 # btn_frm.rowconfigure(0, weight=1, minsize=25)
 # btn_frm.columnconfigure(0, weight=1, minsize=25)
@@ -27,6 +41,6 @@ Q_button.grid(padx=5, pady=5, sticky="w", column=0, row=0)
 S_button.grid(column=1, row=0, padx=5, pady=5)
 
 
-root.resizable(width=False, height=False)
+main_window.resizable(width=False, height=False)
 
-root.mainloop()
+main_window.mainloop()
