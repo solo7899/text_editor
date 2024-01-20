77789b69ca825441812c0738b96edebb26755f59
hamidreza <dolati.hamidreza78@gmail.com>
2024-01-20 13:06:44 +0330

    finished a basic gui, now time to add event-handler, then add more functionality

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-import tkinter as tk
-from tkinter import ttk
+import tkinter as tk 
+from tkinter import ttk, font
 
 root = tk.Tk()
 frm = ttk.Frame(root, padding=10)
 # frm.grid()
-frm.pack(fill=tk.BOTH, expand=True)
+frm.pack(fill="both" ,side="top", expand=True)
+
+fontObj = font.Font(size=18)
 
 label = ttk.Label(master=frm, text="Hello World", anchor="center") #.grid(column=0, row=0)
-text = tk.Text(master=frm)
+text = tk.Text(master=frm, font=fontObj)
 
 btn_frm = ttk.Frame(master=frm)
-button = ttk.Button(master=btn_frm, text="Quit", command=root.destroy) #.grid(column=2, row=0)
+Q_button = ttk.Button(master=btn_frm, text="Quit", command=root.destroy) #.grid(column=2, row=0)
+S_button = ttk.Button(master=btn_frm, text="Save") #.grid(column=2, row=0)
 
-label.pack(fill=tk.BOTH)
-text.pack(fill=tk.BOTH, side=tk.LEFT)
-btn_frm.pack(fill=tk.BOTH)
 
-button.pack(padx=5)
+label.pack(fill="both")
+text.pack(fill="both", side="top", expand=True)
+
+# btn_frm.rowconfigure(0, weight=1, minsize=25)
+# btn_frm.columnconfigure(0, weight=1, minsize=25)
+
+btn_frm.pack(fill="both", side="top", expand=True)
+Q_button.grid(padx=5, pady=5, sticky="w", column=0, row=0)
+S_button.grid(column=1, row=0, padx=5, pady=5)
+
 
 root.resizable(width=False, height=False)
 
